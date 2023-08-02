95229258804ad08a38812d348cd80908e20213e6
gautamsingh1998 <119399501+gautamsingh1998@users.noreply.github.com>
2023-08-02 10:21:01 +0000

    variable

---
+++ 01_bascis/variable.js
@@ -0,0 +1,19 @@
+/* x = 5;
+y = 6;
+z = x + y;
+console.log(z); */
+
+// Use of Let Varible
+
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+}
+console.log(x);
+
+// Use of Var variable
+const PI = 3.141592653589793;
+console.log(PI);
