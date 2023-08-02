66fdd413d70c8f53a612e2e4a28ab2a959f06c94
gautamsingh1998 <119399501+gautamsingh1998@users.noreply.github.com>
2023-08-02 09:54:30 +0000

    flat

--- 01_bascis/test.js
+++ 01_bascis/test.js
@@ -1,3 +1,24 @@
 console.log("Hello, Gautam Singh");
 const str = "Gautam Singh"
 console.log(str);
+
+
+let arr1 = [1, 2, 3, [4, 5], [6, 7, [9, 10]]];
+
+function flattenArray(arr) {
+  let flattened = [];
+
+  arr.forEach((element) => {
+    if (Array.isArray(element)) {
+      flattened = flattened.concat(flattenArray(element));
+      console.log()
+    } else {
+      flattened.push(element);
+    }
+  });
+
+  return flattened;
+}
+
+let flattenedArray = flattenArray(arr1);
+console.log(flattenedArray);
