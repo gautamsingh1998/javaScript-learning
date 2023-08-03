b541630b95bee41b77b173f7353fe634bd139f19
gautamsingh1998 <119399501+gautamsingh1998@users.noreply.github.com>
2023-08-03 08:19:32 +0000

    variable comprision

---
+++ 01_bascis/comparison.js
@@ -0,0 +1,13 @@
+age = 18
+let voteable = ( age <= 18) ? "Not allow to vote":"allow to vote";
+console.log(voteable);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("0" + 0);
+console.log("0" == 0);
+console.log(0 + 4+"0");
+
+console.log("2" === 2);
