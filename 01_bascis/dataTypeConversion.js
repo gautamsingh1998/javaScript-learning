b6b0f3b435203aa507b199705da931c94d96d25e
gautamsingh1998 <119399501+gautamsingh1998@users.noreply.github.com>
2023-08-03 07:22:04 +0000

    data type conversion

--- 01_bascis/dataTypeConversion.js
+++ 01_bascis/dataTypeConversion.js
@@ -19,3 +19,38 @@ let somenumber = 33
 let stringNumber = String(somenumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************************OPERATIONS******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2*2);
+//console.log(2-2);
+//console.log(2**2);
+
+let str1 = "Hello"
+let str2 = " Gautam Singh"
+let str3 =  str1 + str2
+//console.log(str3);
+
+//console.log("1"+5);
+//console.log(1+"5");
+//console.log("1"+"5");
+//console.log("1"+2+5);
+//console.log(1+2+"5");
+
+let str4 = str1 - str2
+console.log(str4);
+
+//console.log(1+3*4/2);
+
+console.log(+true);
+//console.log(true+);
+
+let gameCounter = 100
+gameCounter++;
+gameCounter = gameCounter * 2;
+console.log(gameCounter);
